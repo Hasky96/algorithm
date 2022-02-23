631a9b0f2da6a6a7be222b4f703147c02ff9d547
HaseokJang <dean@HaseokJangs-MacBook-Air.local>
2022-02-23 22:24:38 +0900

    2022.02.23

--- Boj/10158_개미.py
+++ Boj/10158_개미.py
@@ -3,12 +3,13 @@ w, h = map(int, input().split())
 p, q = map(int, input().split())
 t = int(input())
 
+# x 축
 quotient, remainder = divmod((p + t), w)
 if quotient%2:
     x = w - remainder
 else:
     x = remainder
-
+# y 축
 quotient, remainder = divmod((q + t), h)
 if quotient%2:
     y = h - remainder
