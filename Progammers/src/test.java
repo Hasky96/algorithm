565c1ea5bbac54eb9e18cfeb4df692012929d9ca
HaseokJang <dean@HaseokJangs-MacBook-Air.local>
2022-03-13 22:03:36 +0900

    upload

---
+++ Progammers/src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        String a = "a";
+        int n = 5;
+        System.out.println(String.format("%"+n+"s", a ));
+
+
+    }
+}
