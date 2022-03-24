f8d4775353fd16c0a5a593ab5134ddfe0ac0f681
Haseok Jang <haseok1020@naver.com>
2022-03-24 22:03:03 +0900

    2022.03.24 uploads

--- Boj/DFS스페셜저지.java
+++ Boj/DFS스페셜저지.java
@@ -2,53 +2,76 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
-public class DFS스페셜저지{
-    static String ret = "0";
-    static String answer;
-    static String ans = "1 ";
-    static int depth = 1;
-    static int N;
+public class DFS스페셜저지 {
+
+    static ArrayList<ArrayList<Integer>> adj;
     static boolean[] visited;
-    static ArrayList<ArrayList> arr = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        visited = new boolean[N+1];
-        for(int i = 0 ; i <= N ; ++i) arr.add(new ArrayList<>());
-        for(int i = 1 ; i<N; i++){
-            String[] temp = br.readLine().split(" ");
-            arr.get(Integer.parseInt(temp[0])).add(Integer.parseInt(temp[1]));
-            arr.get(Integer.parseInt(temp[1])).add(Integer.parseInt(temp[0]));
-        }
-        answer = br.readLine()+ " ";
-        dfs(1);
-        System.out.println(ret);
-    }
-    public static int dfs(int start){
-        if(depth == N){
-            System.out.println(ans);
-            depth = 1;
-            ans = "1 ";
-            visited = new boolean[N+1];
-            visited[1] = true;
-        }
-        visited[start] = true;
-        System.out.println(ans);
-        if(answer.equals(ans)){
-            ret = "1";
-        }
-        else{
-            for(int i = 1;i<=N;i++){
-                if(!visited[i] && arr.get(start).contains(i)){
-                    ans += i + " ";
-                    depth += 1;
-                    dfs(i);
+    static int[] answer;
+    static boolean flag;
+    static int N, idx;
 
-                }
-            }
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+
+        adj = new ArrayList<>();
+        for(int i = 0 ; i <= N ; ++i) adj.add(new ArrayList<>());
+
+        visited = new boolean[N + 1];
+        answer = new int[N];
+        flag = true;
+        idx = 1;
+
+        for(int i = 0 ; i < N - 1 ; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            adj.get(from).add(to);
+            adj.get(to).add(from);
         }
-        return 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; ++i) answer[i] = Integer.parseInt(st.nextToken());
+
+        if(answer[0] != 1) {
+            System.out.println(0);
+            return;
+        }
+
+        dfs(1);
+
+        if(flag) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+    }
+
+    private static void dfs(int x) {
+        if(visited[x]) return;
+
+        // x정점 방문
+        visited[x] = true;
+
+        ArrayList<Integer> set = new ArrayList<>();
+        for(int next : adj.get(x)) {
+            if(visited[next]) continue;
+            set.add(next);
+        }
+
+        if(set.size() == 0) return;
+
+        if(set.contains(answer[idx])) {
+            dfs(answer[idx++]);
+        } else {
+            flag = false;
+        }
     }
 }
