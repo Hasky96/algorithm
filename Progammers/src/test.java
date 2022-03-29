b5ec741805039deff4a211ad4b1477454325a4e9
Haseok Jang <haseok1020@naver.com>
2022-03-29 23:42:33 +0900

    parking fee solve

--- Progammers/src/test.java
+++ Progammers/src/test.java
@@ -3,20 +3,33 @@ import java.util.regex.Pattern;
 
 public class test {
     public static void main(String[] args) {
-        String a = "a";
-        int n = 5;
-        System.out.println(String.format("%"+n+"s", a ));
-        String s = "dfqowi923eofi39";
-        Pattern pattern = Pattern.compile("[0-9]");
-        Matcher matcher = pattern.matcher(s);
-        while (matcher.find()) {
-            String m = matcher.group();
-            System.out.println(m);
-        }
-        System.out.println("수정");
+        System.out.println(Integer.parseInt("000003"));
 
     }
 }
+//        String a = "a";
+//        int n = 5;
+//        System.out.println(String.format("%"+n+"s", a ));
+//        String s = "dfqowi923eofi39";
+//        Pattern pattern = Pattern.compile("[0-9]");
+//        Matcher matcher = pattern.matcher(s);
+//        while (matcher.find()) {
+//            String m = matcher.group();
+//            System.out.println(m);
+//        }
+//        System.out.println("수정");
+//
+//        loop1 : for(int i=9; i < 14; i++){
+//            for(int j =1;j < 100;j++){
+//                System.out.print(i);
+//                System.out.println(j);
+//                if(j==12) break loop1;
+//            }
+//
+//        }
+//
+//    }
+//}
 /*
 *     o
 *    xox
