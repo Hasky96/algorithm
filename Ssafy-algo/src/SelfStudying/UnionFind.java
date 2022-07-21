20ae9e9b4e3ca3baa8206135ebf40d3d7d825d7b
Haseok Jang <haseok1020@naver.com>
2022-07-21 23:50:27 +0900

    2022.07.21 uploads

---
+++ Ssafy-algo/src/SelfStudying/UnionFind.java
@@ -0,0 +1,21 @@
+package SelfStudying;
+
+public class UnionFind {
+
+    //UnionFind => Disjoint-Set ( 서로소 집합 )
+
+    static int size;
+    static int[] rep;
+
+    static void make(int size){
+        rep = new int[size];
+        for(int i=0; i<size; i++){
+            rep[i] = 0;
+        }
+    }
+
+    static int find(int idx){
+        int []
+        return find(idx)
+    }
+}
