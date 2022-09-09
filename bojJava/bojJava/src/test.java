84a0286dee9eb9646daf2b98cc9933fd168d2ca9
Haseok Jang <haseok1020@naver.com>
2022-09-09 21:07:47 +0900

    2022.09.09 algo

--- bojJava/bojJava/src/test.java
+++ bojJava/bojJava/src/test.java
@@ -1,4 +1,6 @@
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class test {
 
@@ -10,16 +12,23 @@ public class test {
 //        sb.append("aaa\naa") ;
 //        sb.replace(4,5,"b");
 //        System.out.println(sb.toString());
-        System.out.println();
-        TreeMap<String, Integer> t = new TreeMap();
-        t.put("ab", 12);
-        t.put("ac", 13);
-        t.put("a", 1);
-        t.put("ad", 14);
-        System.out.println(t.toString());
-        t.pollFirstEntry();
-        t.pollLastEntry();
+//        System.out.println();
+//        TreeMap<String, Integer> t = new TreeMap();
+//        t.put("ab", 12);
+//        t.put("ac", 13);
+//        t.put("a", 1);
+//        t.put("ad", 14);
+//        System.out.println(t.toString());
+//        t.pollFirstEntry();
+//        t.pollLastEntry();
+//
+//        System.out.println(t.toString());
+        String a = "문현역";
+        Pattern pattern = Pattern.compile(".*\\s?[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|0-9]+역\\s?.*");
+        Matcher matcher = pattern.matcher(a);
 
-        System.out.println(t.toString());
+        if(matcher.find()){
+            System.out.println(matcher.group());
+        }
     }
 }
