609e822aa29f306a10c1942d2c8d4e8cf132d1ec
Haseok Jang <haseok1020@naver.com>
2022-09-22 22:19:43 +0900

    2022.09.22 algo

---
+++ bojJava/bojJava/src/boj1927_minHeap.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class boj1927_minHeap {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(input.readLine());
+
+        PriorityQueue<Integer> Q = new PriorityQueue<Integer>();
+        StringBuilder sb =new StringBuilder();
+        while(N-->0){
+            int x = Integer.parseInt(input.readLine());
+            if(x<1){
+                if(Q.isEmpty()) sb.append(0).append("\n");
+                else sb.append(Q.poll()).append("\n");
+            }else Q.offer(x);
+        }
+        System.out.println(sb.toString());
+    }
+}
